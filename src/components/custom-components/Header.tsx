00630b3057331db6dbdae5296bb28d511202c7a7
Justin-K-Ellis <jlke45@gmail.com>
2025-02-10 21:19:41 +0900

    Add spacing to Header component.

--- src/components/custom-components/Header.tsx
+++ src/components/custom-components/Header.tsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 export default function Header() {
   return (
-    <header className="flex justify-between p-2 items-center">
+    <header className="flex justify-between px-2 py-8 m-8 items-center border-solid border-b border-primary">
       <h1 className="text-3xl font-bold">Logical Space</h1>
       <Navbar />
     </header>
